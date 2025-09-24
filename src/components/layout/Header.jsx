a5280d1ae665d4157f1ede023304992e29db3aa3
JonasPC18 <134112450+JonasPC18@users.noreply.github.com>
2025-09-24 19:13:25 -0300

    aplicação de estilos iniciada

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,14 +2,16 @@ import logo from '../../assets/Logo.svg';
 
 export default function Header() {
   return (
-    <div>
+    <div className="header site-grid">
         <img src={logo} alt="Little Lemon Logo" />
-        <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#menu">Menu</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#order-online">Order Online</a></li>
-        </ul>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#order-online">Order Online</a></li>
+          </ul>
+        </nav>
     </div>)
 }
--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -2,13 +2,14 @@ import restaurantfood from '../../assets/images/restauranfood.jpg';
 
 export default function Hero() {
   return (
-    <div>
-        <hr/>
-        <h1>Little Lemon</h1>
-        <h3>Chicago</h3>
-        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-        <img src={restaurantfood} alt="Restaurant food" width="200px"/> {/* Ajustar depois */}
-        <button>Reserve a Table</button>
-        <hr/>
+    <div className="hero site-grid">
+
+        <div className="hero-text">
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean  restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button>Reserve a Table</button>
+        </div>
+        <img src={restaurantfood} alt="Restaurant food"/>
     </div>)
 }
